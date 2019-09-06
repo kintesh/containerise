9bd54f5743b881983a621380a0050cadc7225a93
LoveIsGrief <loveisgrief@tuta.io>
2019-09-06 16:52:02 +0200

    Rename variables to use camelCase

--- src/ui-preferences/PreferenceGroup.js
+++ src/ui-preferences/PreferenceGroup.js
@@ -7,16 +7,16 @@ export default class PreferenceGroup {
    *
    * @param prefix {String} The prefix to be used for all keys of the preferences
    *                e.g windowOptions --> preferences will be windowOptions.optionX
-   * @param ui_title {String} The title to be shown to the user
-   * @param ui_description {String} The description to be shown to the user
+   * @param uiTitle {String} The title to be shown to the user
+   * @param uiDescription {String} The description to be shown to the user
    * @param preferences {Preference[]}
    * @param toggleable {boolean} Indicates whether the preferences
    *                    can be toggled together
    */
-  constructor(prefix, ui_title, ui_description, preferences, toggleable=false){
+  constructor(prefix, uiTitle, uiDescription, preferences, toggleable=false){
     this.prefix = prefix;
-    this._ui_title = ui_title;
-    this._ui_description = ui_description;
+    this._uiTitle = uiTitle;
+    this._uiDescription = uiDescription;
     this._preferences = preferences;
     this._toggleable = toggleable;
     this._enabled = false;
