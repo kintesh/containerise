81aff79dc3d76869a935f945be0c18ada158f860
LoveIsGrief <loveisgrief@tuta.io>
2020-04-11 15:53:20 +0200

    container-crud: Start connecting create-edit buttons
    Saving and creating are dummy actions at the moment.

--- src/ui/actions/CreateEditAction.js
+++ src/ui/actions/CreateEditAction.js
@@ -1,3 +1,5 @@
+import {makeActionSelectedTrigger} from './utils';
+
 export const COLORS = [
   'blue',
   'turquoise',
@@ -26,16 +28,49 @@ export const ICONS = [
   'fence',
 ];
 
-const $colorSelector = document.querySelector('.container-action.action-create-edit .color-selector');
-const $iconSelector = document.querySelector('.container-action.action-create-edit .icon-selector');
+const $container = document.querySelector('.container-action.action-create-edit');
+const $colorSelector = $container.querySelector('.color-selector');
+const $iconSelector = $container.querySelector('.icon-selector');
+
+const $input = $container.querySelector('input');
+const $buttonDone = $container.querySelector('.action-button.done');
+const $buttonCancel = $container.querySelector('.action-button.cancel');
 
 class CreateEditAction {
 
   constructor() {
+    this.adding = true;
+    this.oldValue = null;
+
+    this.fieldGetters = {};
+
+    this._initFieldGetters();
     this._fillColors();
     this._fillIcons();
+    this._connect();
   }
 
+  _initFieldGetters() {
+    this.fieldGetters['name'] = () => $input.value;
+    this.fieldGetters['color'] = this._getSelected.bind(this, $colorSelector);
+    this.fieldGetters['icon'] = this._getSelected.bind(this, $iconSelector);
+  }
+
+  _getSelected($selector) {
+    let selected = $selector.querySelector('selected');
+    if (selected) {
+      selected = selected.dataset.icon;
+    }
+    return selected;
+  }
+
+  _connect() {
+    $input.addEventListener('change', () => {
+      $buttonDone.disabled = !$input.value.trim();
+    });
+    $buttonDone.addEventListener('click', this.onDone.bind(this));
+    makeActionSelectedTrigger($buttonCancel);
+  }
 
   _fillColors() {
     for (let color of COLORS) {
@@ -48,7 +83,7 @@ class CreateEditAction {
 
   _fillIcons() {
     this._createIconColorStyles();
-    let i=0;
+    let i = 0;
     for (let icon of ICONS) {
       // Icon container contains a background set in CSS
       let $iconContainer = document.createElement('div');
@@ -70,7 +105,7 @@ class CreateEditAction {
    * Generate a style that allows using a data-attribute to set the icon color
    * @private
    */
-  _createIconColorStyles(){
+  _createIconColorStyles() {
     let $style = document.createElement('style');
     for (let color of COLORS) {
       $style.innerHTML += `
@@ -81,6 +116,44 @@ class CreateEditAction {
     }
     $iconSelector.appendChild($style);
   }
+
+  create() {
+    console.log('creating', this.getObject());
+  }
+
+  save() {
+    console.log('saving', this.getObject());
+  }
+
+  canSave() {
+    let saveWorthy = true;
+    for (let fieldName of Object.keys(this.fieldGetters)) {
+      let value = this.fieldGetters[fieldName]();
+      saveWorthy &= !!value && (this.oldValue ?
+              this.oldValue[fieldName] !== value :
+              true
+      );
+      if (!saveWorthy) {
+        break;
+      }
+    }
+    return saveWorthy;
+  }
+
+  getObject() {
+    Object.keys(this.fieldGetters).reduce((acc, curr) => {
+      acc[curr] = this.fieldGetters[curr]();
+      return acc;
+    }, {});
+  }
+
+  onDone() {
+    if (!this.canSave()) {
+      return;
+    }
+    this.oldValue ? this.save() : this.create();
+  }
+
 }
 
 
